744d8a9f687d084ae8848553075e8877bae8f680
Dionne Stratton <mylife33@hotmail.com>
2020-11-03 18:53:58 -0600

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import { removeFeature } from "../actions.js";
+import { connect } from "react-redux";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
+  const handler = (event) => {
+    event.preventDefault();
+    props.removeFeature(props.feature);
+  };
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={handler}>
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
